a7f2f90232c237fd07a00169b2dfd03704f6d127
MohamedAliHmem <dalihmem47@gmail.com>
2025-09-24 16:40:00 +0100

    fix: update baseURL for SEO and adjust build scripts for Open Graph image generation

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,8 @@ const withMDX = mdx({
 const nextConfig = {
   output: 'export',
   trailingSlash: true,
+  basePath: '/fawzi_portfolio',
+  assetPrefix: '/fawzi_portfolio/',
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
